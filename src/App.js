937b321be916bb8511cafc0588d28e3596980f85
AlexanderSeliakov <46649919+AlexanderSeliakov@users.noreply.github.com>
2020-06-12 15:07:08 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import Header from './Shared/Header/Header'
+import Alert from './Shared/Alert/Alert'
+import Home from './Home/Home'
+import About from './About/About'
+import AlertState from './Context/Alert/AlertState';
+import FbState from './Context/FireBase/FbState';
+import './App.css';
+
+function App() {
+  return (
+    <FbState>
+      <AlertState>
+        <Router>
+          <Header/>
+          <main>
+            <Alert/>
+            <Switch>
+              <Route path='/' exact>
+                <Home/>
+              </Route>
+              <Route path='/about' exact>
+                <About/>
+              </Route>
+            </Switch>
+          </main>
+        </Router>
+      </AlertState>
+    </FbState>
+  );
+}
+
+export default App;
